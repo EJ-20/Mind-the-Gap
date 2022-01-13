8bb205134bf89024ba3d19df15ff066760d09056
Erfan Jafar <67335103+EJ-20@users.noreply.github.com>
2022-01-13 12:23:33 -0330

    changes

--- GUI.java
+++ GUI.java
@@ -10,13 +10,29 @@ public class GUI extends JFrame implements ActionListener, MouseListener {
     private JPanel topPanel, bottomPanel;
     private JLabel instructionLabel;
     private JButton topButton;
-    private GridSquare[][] gridSquares;
+    private GridSquare [][] gridSquares;
     private int rows, columns;
 
 
     public GUI() {
         rows = 4;
         columns = 4;
+
+        topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+
+        bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
+
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(topPanel, BorderLayout.NORTH);
+        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        setVisible(true);
+
 
     }
 
