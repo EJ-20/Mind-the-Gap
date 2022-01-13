d102d87f9e34ca0dc4f38cf2658eccf9ad3d827e
Erfan Jafar <67335103+EJ-20@users.noreply.github.com>
2022-01-13 17:52:38 -0330

    comit

--- GUI.java
+++ GUI.java
@@ -12,22 +12,20 @@ public class GUI extends JFrame implements ActionListener {
     private JPanel topPanel, bottomPanel;
     private JLabel instructionLabel;
     private JButton topButton;
-    private JButton whiteButton;
-    private JButton redButton;
-    private JButton blueButton;
-    private GridLayout gridSquares;
-    private int rows, columns;
+    private GridSquare [][] gridSquares;
+    private int x, y;
 
     // create, organize and display the GUI
     public GUI() {
-
-        rows = 4;
-        columns = 4;
 
         // creating the panels as required
         topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout());
         bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout());
+
+        x = 4;
+        y = 4;
 
 
         instructionLabel = new JLabel("Player's turn");
@@ -38,24 +36,22 @@ public class GUI extends JFrame implements ActionListener {
         topPanel.add(instructionLabel);
         topPanel.add(topButton);
 
-        GridLayout grid = new GridLayout(rows, columns);
-        bottomPanel.setLayout(grid);
 
 
-        whiteButton = new JButton();
-        blueButton = new JButton();
-        redButton = new JButton();
+        gridSquares = new GridSquare [x][y];
+        for(int column = 0; column < x; column++){
+            for(int row = 0; row < y; row++){
+                gridSquares [column][row] = new GridSquare( x,y);
+                gridSquares [column][row].setSize( 20, 20);
+                gridSquares [column][row].setColor( column + row);
+                gridSquares [column][row].setOpaque( true);				// without this line and the next the OS' default
+                //gridSquares [column][row].setBorderPainted( false);		// look & feel will dominate / interfere
+                // (try commenting each out & see what happens)
 
-        whiteButton.setBackground(Color.WHITE);
-        whiteButton.setOpaque(true);
+                bottomPanel.add( gridSquares [column][row]);
 
-
-
-        /*for(int i = 0; i <= rows; i++){
-            for(int x = 0; x <= columns; x++){
-                bottomPanel.add(whiteButton);
             }
-        }*/
+        }
 
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(topPanel, BorderLayout.NORTH);
